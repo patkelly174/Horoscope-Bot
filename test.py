21a64eda2c2225b154a756c50504ce5a0025d4cf
mkirslis <79292610+mkirslis@users.noreply.github.com>
2021-02-18 20:58:23 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
 print("hello")
+
+# zodiac signs
+
+import datetime
+
+currentDate = datetime.datetime.now(%d)
+
+print(currentDate)
+
+  s1 = ["Aries, "]
+  s2 = ["Taurus"]
+  s3 = ["Gemini"]
+  s4 = ["Cancer"]
+  s5 = ["Leo"]
+  s6 = ["Virgo"]
+  s7 = ["Libra"]
+  s8 = ["Scorpio]
+  s9 = ["Sagittarius"]
+  s10 = ["Capricorn"]
+  s11 = ["Aquarius"]
+  s12 = ["Pisces"]
